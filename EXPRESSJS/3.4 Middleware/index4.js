d3c76949bb0d13ffc8a8febb0537759875ccff4f
brandom95 <brandom.ochoa95@gmail.com>
2024-09-23 10:24:07 -0400

    september 23

--- EXPRESSJS/3.4 Middleware/index4.js
+++ EXPRESSJS/3.4 Middleware/index4.js
@@ -1,7 +1,40 @@
 import express from "express";
+import morgan from "morgan";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const port = 3000;
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use(express.urlencoded({ extended: true }));
+
+const logger = (req, res, next) => {
+  if (req.method === "POST") {
+    const userInputStreet = req.body.street;
+    const userInputPet = req.body.pet;
+
+    console.log('requested input', [userInputPet, userInputStreet]
+    );
+    if (!userInputStreet || !userInputPet) {
+      return res.status(400).send('Both fields are required !');
+    }
+  }
+  next();
+};
+
+app.use(logger);
+
+app.post("/submit", logger, (req, res) => {
+  const userInputStreet = req.body.street;
+  const userInputPet = req.body.pet;
+  res.send(`<h1> Your band name is: </h1> <h2>${userInputStreet}${userInputPet} 👌</h2>`)
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
